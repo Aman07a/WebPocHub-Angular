eef14244f7e3045015941eb2529b99df9fd290d4
Aman Ahmed <aman16.aa17@gmail.com>
2023-02-13 14:47:44 +0100

    Testing ASP.NET Core Web API endpoints - Part 2

--- src/app/components/employees-list/employees-list.component.ts
+++ src/app/components/employees-list/employees-list.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Subscription } from 'rxjs';
+
+import { EmployeesService } from 'src/app/services/employees.service';
+import { Employee } from 'src/app/models/employee';
 
 @Component({
   selector: 'wph-employees-list',
@@ -7,4 +11,15 @@ import { Component } from '@angular/core';
 })
 export class EmployeesListComponent {
 
+  constructor(private _employeesService: EmployeesService) {
+
+  }
+
+  title: string = "Welcome To WebPoCHub Employees List!";
+  subTitle: string = "Core Development Members Of WebPoCHub!";
+  employees: Employee[];
+
+  ngOnInit(): void {
+    this._employeesService.getAllEmployees().subscribe(data => this.employees = data);
+  }
 }
--- src/app/services/employees.service.ts
+++ src/app/services/employees.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Observable } from 'rxjs';
+
+import { Employee } from '../models/employee';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeesService {
 
-  constructor() { }
+  constructor(private _httpClient: HttpClient) { 
+    
+  }
+
+  private _baseAddress: string = "https://localhost:7274/api";
+
+  getAllEmployees(): Observable<Employee[]> {
+    return this._httpClient.get<Employee[]>(`${this._baseAddress}/employees`, {
+      headers: {
+        "Authorization": "Bearer eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiZGFiYWRlLnByYXZpbmt1bWFyQHdlYnBvY2h1Yi5jb20iLCJyb2xlIjoiRW1wbG95ZWUiLCJuYmYiOjE2NzYyOTQ4NTAsImV4cCI6MTY3NjI5ODQ1MCwiaWF0IjoxNjc2Mjk0ODUwfQ.SUwtLtdZuK5Wc82CE9aCA-uBJoD7t8lR5fz6YfXx5kZVgCl9zq-6kUJb9bBt9BzoEhfsgPK5VGl5ISdUsBNPfQ"
+      }
+    });
+  }
 }
